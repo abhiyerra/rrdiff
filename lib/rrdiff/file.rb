3480f564500bbafcd606c999b9fdb795d839119c
Alex MacCaw <maccman@gmail.com>
2010-02-25 22:57:04 +0000

    fix argument order bug

--- lib/rrdiff/file.rb
+++ lib/rrdiff/file.rb
@@ -4,27 +4,27 @@ module RRDiff
   module File
     # File interface to RRDiff
     
-    def patch(delfile, file)
-      newfile = Tempfile.new("newfile")
-      RRDiff.patch(delfile.path, file.path, newfile.path)
-      newfile
+    def patch(ofile, dfile)
+      nfile = Tempfile.new("nfile")
+      RRDiff.patch(ofile.path, dfile.path, nfile.path)
+      nfile
     end
 
     def signature(file)
-      sigfile = Tempfile.new("sigfile")
-      RRDiff.signature(file.path, sigfile.path)
-      sigfile
+      sfile = Tempfile.new("sigfile")
+      RRDiff.signature(file.path, sfile.path)
+      sfile
     end
 
-    def delta(sigfile, newfile)
-      delfile = Tempfile.new("delfile")
-      RRDiff.delta(newfile.path, sigfile.path, delfile.path)
-      delfile
+    def delta(file, sfile)
+      dfile = Tempfile.new("delfile")
+      RRDiff.delta(file.path, sfile.path, dfile.path)
+      dfile
     end
 
     def diff(ofile, nfile)
-    	sig = signature(ofile)
-    	delta(sig, nfile)
+    	sfile = signature(ofile)
+    	delta(sfile, nfile)
     end
     extend self
   end
